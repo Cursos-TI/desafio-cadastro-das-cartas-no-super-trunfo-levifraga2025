67be3be621fbc9b23b72fb084633fb4b560a81e0
levifraga2025 <jrtagima@gmail.com>
2025-08-21 02:37:50 +0000

    primeiro teste carta 1 ok

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 
-int main(){
+int main() {
     // Carta 1 - Ceará
-    char codigo1[4] = "A01";
-    char estado1[20] = "Ceara";
-    char cidade1[30] = "Fortaleza";
-    int populacao1 = 12300000;
-    float area1 = 1521;
-    float pib1 = 750;
-    int pontosTuristicos1 = 20;
-    
-    // Carta 2 - Piauí
-    char codigo2[10] ="B01";
-    char estado2[22] = "Piauí";
-    char cidade2[60] ="Teresina";
-    int populacao2 = 9070000;
-    float area2 = 250;
-    float pib2 = 52;
-    int pontosTuristicos2 = 4;
+    char codigoCarta[4] = "A01";
+    char estado[20] = "Ceará";
+    char cidade[30] = "Fortaleza";
+    float populacao = 12300000;
+    float areaKm2 = 1521;
+    float pib = 750;
+    int pontosTuristicos = 20;
 
-    
+    printf("=== Carta Super Trunfo ===\n");
+    printf("Código da carta: %s\n", codigoCarta);
+    printf("Estado: %s\n", estado);
+    printf("Cidade: %s\n", cidade);
+    printf("População: %.0f habitantes\n", populacao);
+    printf("Área: %.2f km²\n", areaKm2);
+    printf("PIB: %.2f bilhões de R$\n", pib);
+    printf("Número de pontos turísticos: %d\n", pontosTuristicos);
+
     return 0;
 }
